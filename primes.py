9b52fabc1fc15725dedb7eec8608ff49757d777b
GeriVoodCoder <riazwaan03@gmail.com>
2022-10-02 22:05:37 +0100

    Solution to primes-v2

--- primes.py
+++ primes.py
@@ -2,5 +2,19 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError
     list = []
+    prime = 2
+    while 1:
+        if len(list) < number_of_primes:
+            for i in range(2,prime):
+                if prime % i == 0:
+                    prime += 1
+                    break
+            else:
+                list.append(prime)
+                prime += 1
+        else:
+            break
     return list
